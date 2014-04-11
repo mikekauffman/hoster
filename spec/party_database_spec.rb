1c74f5c92f13ceca784014eb101fdf598dc21557
mikekauffman <mike.a.kauffman@gmail.com>
2014-04-11 10:25:51 -0600

    fixed tests to reflect change of remove button functionality

--- spec/party_database_spec.rb
+++ spec/party_database_spec.rb
@@ -17,19 +17,19 @@ describe PartyDatabase do
     party_table.create({name: 'Mike', size: 4, phone: 7033718749})
     party_table.create({name: 'Dave', size: 2, phone: 7035554848})
     expect(party_table.all).to eq [
-                                     {name: 'Mike', size: 4, phone: 7033718749, :arrival_time=>nil},
-                                     {name: 'Dave', size: 2, phone: 7035554848, :arrival_time=>nil},
+                                    {name: 'Mike', size: 4, phone: 7033718749, :arrival_time => nil},
+                                    {name: 'Dave', size: 2, phone: 7035554848, :arrival_time => nil},
 
-                                   ]
+                                  ]
   end
 
   it 'allows a party to be removed from the waiting list' do
     party_table = PartyDatabase.new(DB)
     party_table.create({name: 'Mike', size: 4, phone: 7033718749})
     party_table.create({name: 'Dave', size: 2, phone: 7035554848})
-    party_table.delete('Mike')
+    party_table.delete(7033718749)
     expect(party_table.all).to eq [
-                                    {name: 'Dave', size: 2, phone: 7035554848, :arrival_time=>nil}
+                                    {name: 'Dave', size: 2, phone: 7035554848, :arrival_time => nil}
                                   ]
   end
 
